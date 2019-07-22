cd38de13da723301dc7a1f6e73f9280a48acb6f5
Nemanja Savkic <nemanja.savkic92@gmail.com>
2019-07-22 13:25:54 +0200

    - Added weather service

--- AirVironment/src/app/weather/weather.component.ts
+++ AirVironment/src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Weather } from './weather.model';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  weather: Weather;
+  interval: any;
 
-  ngOnInit() {
+  constructor(private weatherService: WeatherService) { }
+
+  getWeatherData(): void {
+    this.weatherService.getWeatherData().subscribe(
+      weather => {
+        this.weather = weather;
+      }, 
+      () => console.error('Error'));
   }
 
+  ngOnInit() {
+    this.getWeatherData();
+    this.interval = setInterval(() => {
+      this.getWeatherData();
+    }, 30000);
+  }
 }
--- AirVironment/src/app/weather/weather.service.ts
+++ AirVironment/src/app/weather/weather.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  apiUrl = 'https://boiling-scrubland-99747.herokuapp.com/measurement/latest';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 }
