badee08d9c9badbc9d3695c1c404e0f0a9e950ff
Nemanja Savkic <nemanja.savkic92@gmail.com>
2019-07-23 14:12:53 +0200

    - Added chart

--- AirVironment/src/app/weather/weather.component.ts
+++ AirVironment/src/app/weather/weather.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Weather } from './weather.model';
 import { WeatherService } from './weather.service';
 import { Router, ActivatedRoute } from '@angular/router'
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-weather',
@@ -11,12 +12,14 @@ import { Router, ActivatedRoute } from '@angular/router'
 export class WeatherComponent implements OnInit {
 
   showComp2: boolean = false;
+  chart: any = [];
   weather: Weather;
   interval: any;
   humidity: any = [];
   pollution: any = [];
   temperature: any = [];
   timestamps: any = [];
+  initiated = false;
 
   constructor(private weatherService: WeatherService,
               private router: Router,
@@ -30,8 +33,81 @@ export class WeatherComponent implements OnInit {
         this.temperature.push(this.weather.temperature);
         this.pollution.push(this.weather.pollution);
         this.timestamps.push(new Date(this.weather.timestamp).toLocaleTimeString());
+        if (!this.initiated) {
+          this.setChart();
+          this.initiated = true;
+        }
+        this.chart.update();
       }, 
       () => console.error('Error'));
+  }
+
+  setChart() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels:  this.timestamps,
+        datasets: [
+          {
+            label: 'iaq',
+            data: this.pollution,
+            borderColor: '#3cba9f',
+            backgroundColor: '#3cba9f',
+            fill: false,
+            padding: 10
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Air pollution chart',
+          position: 'right',
+          fontSize: 14,
+          fontColor: '#fff'
+        },
+        layout: {
+          padding: {
+              left: 50,
+              right: 50,
+              top: 50,
+              bottom: 50
+          }
+        },
+        tooltips: {
+          mode: 'point'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            gridLines: {
+              display: true ,
+              color: "#FFFFFF"
+            },
+            ticks: {
+              fontColor: "#fff",
+              autoSkip: true,
+              maxTicksLimit: 5
+            },
+          }],
+          yAxes: [{
+            display: true,
+            gridLines: {
+              display: true ,
+              color: "#FFFFFF",
+            },
+            ticks: {
+              fontColor: "#fff"
+            }
+          }],
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
   }
 
   showChildComp() {
